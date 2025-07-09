5c80ab49c98a45eb9a742e14dabb908b4ee07966
Salim kök <155094272+SalimKok@users.noreply.github.com>
2025-07-09 18:25:33 +0300

    düzenleme
    dosya düzenleme

--- hepsibirarada/hepsibirarada/hebirflask/hebirflask/firebase_config.py
+++
@@ -1,8 +0,0 @@
-# firebase_config.py
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate("firebase_key.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
